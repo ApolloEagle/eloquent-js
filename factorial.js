e5a285db1109990506b23053edd6cb6998402666
Blake Reimer <sobeksea@gmail.com>
2020-07-15 23:13:54 -0600

    factorial.js (Updated)

--- factorial.js
+++ factorial.js
@@ -1,10 +1,3 @@
-function factorial(n) {
-    if(n === 0) {
-        return 1;
-    }
-    else {
-        return factorial(n - 1) * n;
-    }
-}
+const factorial = n => n === 0 ? 1 : (n - 1) * n;
 
 console.log(factorial(3));
