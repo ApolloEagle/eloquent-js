1fdd371f218f0ad500f075c781e2d103eaf6695b
Blake Reimer <sobeksea@gmail.com>
2020-07-19 12:54:08 -0600

    Power function (Updated)

--- power.js
+++ power.js
@@ -1,9 +1,9 @@
-const power = function(base, exponent) {
+const power = (base, exponent) => {
   let result = 1;
   for (let count = 0; count < exponent; count++) {
     result *= base;
   }
   return result;
-};
+}
 
 console.log(power(3, 3));
