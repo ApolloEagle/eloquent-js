be1314b6e039eda32a2d921a7cda297db98ea789
Blake Reimer <sobeksea@gmail.com>
2020-07-19 14:52:07 -0600

    Closure

---
+++ closure.js
@@ -0,0 +1,3 @@
+multiplier = factor => number => number * factor;
+let twice = multiplier(2);
+console.log(twice(5));
---
+++ optional-argument.js
@@ -0,0 +1,10 @@
+ const power = (base, exponent = 2) => {
+  let result = 1;
+  for (let count = 0; count < exponent; count++) {
+    result *= base;
+  }
+  return result;
+}
+
+console.log(power(3));
+console.log(power(3, 3));
