3eea0f7e47e73eccd811a0827c61bce234e3c6d4
Blake Reimer <sobeksea@gmail.com>
2020-07-23 14:36:06 -0600

    Chapter 2 Exercise: Chessboard

---
+++ src/chapter2/chessboard.js
@@ -0,0 +1,10 @@
+let row1 = ' # # # #';
+let row2 = '# # # # ';
+for(let i = 0; i < 8; i++) {
+    if(i % 2 === 0) {
+        console.log(row1);
+    }
+    else {
+        console.log(row2);
+    }
+}
